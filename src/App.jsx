f18baf62e5e6422ed496f7cd99394605b5916abf
devcodersh <devloper.codersh@gmail.com>
2025-03-07 18:10:20 +0530

    create router and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import '../src/assets/css/main.css';
-import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <Header/>
+    <Outlet />
     </>
   )
 }
